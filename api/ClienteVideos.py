97f36b15ba5705872554bcda0cda59f59851a912
adridavila <39356834+adridavila@users.noreply.github.com>
2025-09-15 12:19:19 -0600

    modificacion-A

---
+++ api/ClienteVideos.py
@@ -0,0 +1,56 @@
+import aiohttp
+import os #Libreria para el uso del sistema
+from dotenv import load_dotenv
+from .conexion import Conexion
+
+class ClienteVideos:
+    def __init__(self, conexion: Conexion):
+        self.__conexion = conexion
+        self.__url = self.__conexion.getUrl()
+
+    #Buscar videos por id de canal
+    async def buscarVideoID(self, id_video:int):
+        url = f"{self.__url}/videos/videosCanal"
+        await self.__conexion.conexion( )
+        param = {"id": id_video}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=param) as resp:
+                resp.raise_for_status( )
+                datos=await resp.json()
+                if datos:
+                    return  datos
+                return None
+            
+    #Buscar videos por nombre
+    async def buscarVideoNombre(self, nombre: str):
+        url = f"{self.__url}/videos/buscarVideoNombre/{nombre}"
+        await self.__conexion.conexion( )
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                resp.raise_for_status( )
+                datos= await resp.json()
+                if datos:
+                    return datos
+                return None
+
+    #Buscar videos por url
+    async def buscarVideoUrl(self, url_video: str):
+        url = f"{self.__url}/videos/buscarVideoUrl"
+        await self.__conexion.conexion( )
+        param = {"url": url_video.strip()}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=param) as resp:
+                resp.raise_for_status( )
+                datos= await resp.json()
+                if datos:
+                    return datos
+                return None
+
+    #mostrar todos los videos
+    async def mostrarVideos(self):
+        url = f"{self.__url}/videos/mostrarVideos/"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    return await response.json()
+                return None
--- vistas/Shell.py
+++ vistas/Shell.py
@@ -3,6 +3,8 @@ import shlex #Para limpiar nuestro array de comandos
 import os #Para limpiar la consola
 import getpass #Obtener información del usuariof
 from .Descargas import Descargas
+from .Videos import Videos
+
 
 class Shell:
     
@@ -11,14 +13,20 @@ class Shell:
         self.__subparsers = self.__comandos.add_subparsers(dest="command")
         self.__user=getpass.getuser( )
         self.__descargas=Descargas( )
+        self.__videos= Videos( )
         
-    def comandos(self):    
+    def comandos(self):    #modificar este para crear los comandos 
         # --- Comando descargar videos por archivo ---
         parser_archivo=self.__subparsers.add_parser("download",help="Descargar videos")
         parser_archivo.add_argument("-u", "--url", required=True, help="Descargar videos")
-        
-        videos=self.__subparsers.add_parser("nombre comando",help="Funcion del comando")
-        
+        parser_archivo.add_argument("-b", )
+        # comando para buscar videos por:
+        parser_videos=self.__subparsers.add_parser("search", help="Buscar videos")
+        parser_videos.add_argument ("-n","--nombre", help="Buscar videos nombre")
+        parser_videos.add_argument ("-i","--id", help="Buscar videos ID")
+        parser_videos.add_argument ("-u","--url", help="Buscar videos por url")
+        parser_videos.add_argument ("-a","--all", action="store_true", help="Mostrar todos los videos")
+                
   
     def limpiar_consola(self):
         # Windows
@@ -28,14 +36,36 @@ class Shell:
         else:
             os.system("clear")
         
-    async def funciones(self,comando,args):
+    async def funciones(self,comando,args): #agregar las funciones de nuestro metodo de rutas
           # Ejecutar comando
-          if comando.command=="download":
-                await self.__descargas.descargar(args[2])
-          
-              
-          else:
-                 print("Error en el comando no valido")
+        if comando.command=="download":
+            await self.__descargas.descargar(args[2])
+        elif comando.command == "search":
+            if comando.nombre:
+                datos= await self.__videos.buscarVideoNombre(args[2])
+                print(datos)
+            elif comando.id: 
+                datos1 = await self.__videos.buscarVideoID(args[2])
+                print(datos1)
+            elif comando.url:
+                datos2 = await self.__videos.buscarVideoURL(args[2])
+                print(datos2)
+            elif comando.all:
+                datos3 = await self.__videos.mostrarVideos()
+                print(datos3)
+        else:
+                print("No se reconocio el comando")
+                                    
+        #elif comando.command=="shearch":
+        #await self.__videos.buscarVideoNombre(args[2])
+        #elif comando.command=="sharei":  
+        #await self.__videos.buscarVideoID(args[2])
+        #elif comando.command=="shareu":
+        #await self.__videos.buscarVideoURL(args[2])
+        #elif comando.command=="showall":
+        #await self.__videos.mostrarVideos( )
+        #else:
+        #print("Error en el comando no valido")
     
     async def terminal(self):
         self.comandos( ) #Inicializamos los comandos
@@ -67,7 +97,8 @@ class Shell:
                 
                 args = args[1:]
                 parsed = self.__comandos.parse_args(args)
-                
+                print(args)
+                print(args[2])
                 await self.funciones(parsed,args)
               
             except SystemExit:
