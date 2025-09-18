698963b2e65365f288935b01037b04d5ca5627e4
adridavila <39356834+adridavila@users.noreply.github.com>
2025-09-18 11:46:35 -0600

    Actualizacion metodos video

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # from vistas.Shell import Shell
 from vistas.Shell import Shell
-from vistas.shell_prompt import Shell as ShellPromt
-from vistas.shell_rich import Shell as shellRich
+
 import asyncio
 if __name__ == "__main__":
-    shell=ShellPromt( )
+    shell=Shell( )
     asyncio.run(shell.terminal( ))
